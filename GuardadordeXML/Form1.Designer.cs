e678eb714ed23e51c53c3dd3b1660d49cf017aa4
diego <diego@DGMOLAR-ACER>
2024-06-04 22:57:37 -0600

    Modificaciones

---
+++ GuardadordeXML/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace GuardadordeXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.Titulo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.LimeGreen;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.button1.Location = new System.Drawing.Point(556, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(333, 87);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clic para procesar Factura XML";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Titulo
+            // 
+            this.Titulo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Titulo.AutoSize = true;
+            this.Titulo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Titulo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Titulo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.Titulo.ForeColor = System.Drawing.Color.Black;
+            this.Titulo.Location = new System.Drawing.Point(38, 34);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Padding = new System.Windows.Forms.Padding(5);
+            this.Titulo.Size = new System.Drawing.Size(864, 67);
+            this.Titulo.TabIndex = 2;
+            this.Titulo.Text = "Procesador de XML a Base de Datos";
+            this.Titulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(71, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(420, 320);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(160, 450);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(243, 73);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(556, 113);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(333, 249);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(951, 535);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Titulo);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
---
+++ GuardadordeXML/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Linq; // Agrega esta directiva de uso
+using System.IO;
+using System.IO.Compression;
+
+namespace GuardadordeXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Ruta del archivo ZIP
+            string zipPath = @"C:\Users\diego\Downloads\FacturaTimbrada.zip";
+            string xmlPath = ExtractXMLFromZip(zipPath);
+
+            if (xmlPath == null)
+            {
+                MessageBox.Show("No se encontró un archivo XML en el ZIP.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Leer el XML y obtener los datos
+            XDocument xmlDoc = XDocument.Load(xmlPath);
+            XElement comprobanteElement = xmlDoc.Element(XName.Get("Comprobante", "http://www.sat.gob.mx/cfd/4"));
+            XElement timbreElement = comprobanteElement.Descendants(XName.Get("TimbreFiscalDigital", "http://www.sat.gob.mx/TimbreFiscalDigital")).FirstOrDefault();
+            string uuid = timbreElement.Attribute("UUID").Value;
+
+            // Comprobar si la factura ya existe en la base de datos
+            if (FacturaExiste(uuid))
+            {
+                MessageBox.Show("La factura XML ya ha sido guardada anteriormente dentro de la Base de Datos.");
+            }
+            else
+            {
+                GuardarFacturaEnBaseDeDatos(comprobanteElement, timbreElement);
+                MessageBox.Show("Factura XML guardada con éxito.");
+
+                // Mover el archivo XML procesado a la carpeta "Facturas-Procesadas"
+                string processedXmlFolderPath = @"C:\Users\diego\Downloads\Facturas-Procesadas";
+                Directory.CreateDirectory(processedXmlFolderPath); // Crea la carpeta si no existe
+
+                string newFileName = GenerateUniqueFileName(processedXmlFolderPath, "FacturaTimbrada", "xml");
+                string processedXmlPath = Path.Combine(processedXmlFolderPath, newFileName);
+
+                File.Move(xmlPath, processedXmlPath);
+            }
+        }
+
+        // Método para extraer el archivo XML desde el archivo ZIP
+        private string ExtractXMLFromZip(string zipPath)
+        {
+            string tempFolderPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tempFolderPath);
+
+            try
+            {
+                using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+                {
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                        if (entry.FullName.EndsWith(".xml", StringComparison.OrdinalIgnoreCase))
+                        {
+                            string destinationPath = Path.Combine(tempFolderPath, entry.FullName);
+                            entry.ExtractToFile(destinationPath);
+                            return destinationPath;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al extraer el archivo XML: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return null;
+        }
+
+        // Método para generar un nombre de archivo único
+        private string GenerateUniqueFileName(string folderPath, string baseFileName, string extension)
+        {
+            int fileCount = 1;
+            string newFileName;
+            do
+            {
+                newFileName = $"{baseFileName}-[{fileCount}].{extension}";
+                fileCount++;
+            } while (File.Exists(Path.Combine(folderPath, newFileName)));
+
+            return newFileName;
+        }
+
+        private bool FacturaExiste(string uuid)
+        {
+            string connectionString = "Server=(local); Database=BaseDatosXML; Integrated Security=true;Encrypt=False";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Comprobante WHERE UUID = @UUID", conn);
+                cmd.Parameters.AddWithValue("@UUID", uuid);
+
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+        }
+
+        private void GuardarFacturaEnBaseDeDatos(XElement comprobanteElement, XElement timbreElement)
+        {
+            string connectionString = "Server=(local); Database=BaseDatosXML; Integrated Security=true;Encrypt=False";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Guardar Comprobante
+                SqlCommand cmd = new SqlCommand(
+                    "INSERT INTO Comprobante (UUID, Version, Folio, Fecha, FormaPago, SubTotal, Total, Descuento, MetodoPago, Moneda, TipoCambio, TipoDeComprobante, LugarExpedicion, Exportacion, NoCertificado, Certificado, Sello) " +
+                    "VALUES (@UUID, @Version, @Folio, @Fecha, @FormaPago, @SubTotal, @Total, @Descuento, @MetodoPago, @Moneda, @TipoCambio, @TipoDeComprobante, @LugarExpedicion, @Exportacion, @NoCertificado, @Certificado, @Sello)", conn);
+
+                cmd.Parameters.AddWithValue("@UUID", timbreElement.Attribute("UUID").Value);
+                cmd.Parameters.AddWithValue("@Version", comprobanteElement.Attribute("Version").Value);
+                cmd.Parameters.AddWithValue("@Folio", comprobanteElement.Attribute("Folio")?.Value ?? string.Empty);
+                cmd.Parameters.AddWithValue("@Fecha", DateTime.Parse(comprobanteElement.Attribute("Fecha").Value));
+                cmd.Parameters.AddWithValue("@FormaPago", comprobanteElement.Attribute("FormaPago").Value);
+                cmd.Parameters.AddWithValue("@SubTotal", decimal.Parse(comprobanteElement.Attribute("SubTotal").Value));
+                cmd.Parameters.AddWithValue("@Total", decimal.Parse(comprobanteElement.Attribute("Total").Value));
+                cmd.Parameters.AddWithValue("@Descuento", decimal.Parse(comprobanteElement.Attribute("Descuento")?.Value ?? "0"));
+                cmd.Parameters.AddWithValue("@MetodoPago", comprobanteElement.Attribute("MetodoPago").Value);
+                cmd.Parameters.AddWithValue("@Moneda", comprobanteElement.Attribute("Moneda").Value);
+                cmd.Parameters.AddWithValue("@TipoCambio", decimal.Parse(comprobanteElement.Attribute("TipoCambio")?.Value ?? "0"));
+                cmd.Parameters.AddWithValue("@TipoDeComprobante", comprobanteElement.Attribute("TipoDeComprobante").Value);
+                cmd.Parameters.AddWithValue("@LugarExpedicion", comprobanteElement.Attribute("LugarExpedicion").Value);
+                cmd.Parameters.AddWithValue("@Exportacion", comprobanteElement.Attribute("Exportacion")?.Value ?? string.Empty);
+                cmd.Parameters.AddWithValue("@NoCertificado", comprobanteElement.Attribute("NoCertificado").Value);
+                cmd.Parameters.AddWithValue("@Certificado", comprobanteElement.Attribute("Certificado").Value);
+                cmd.Parameters.AddWithValue("@Sello", comprobanteElement.Attribute("Sello").Value);
+
+                cmd.ExecuteNonQuery();
+
+                // Guardar Emisor
+                XElement emisorElement = comprobanteElement.Element(XName.Get("Emisor", "http://www.sat.gob.mx/cfd/4"));
+                SqlCommand cmdEmisor = new SqlCommand(
+                    "INSERT INTO Emisor (UUID, Nombre, Rfc, RegimenFiscal) " +
+                    "VALUES (@UUID, @Nombre, @Rfc, @RegimenFiscal)", conn);
+                cmdEmisor.Parameters.AddWithValue("@UUID", timbreElement.Attribute("UUID").Value);
+                cmdEmisor.Parameters.AddWithValue("@Nombre", emisorElement.Attribute("Nombre").Value);
+                cmdEmisor.Parameters.AddWithValue("@Rfc", emisorElement.Attribute("Rfc").Value);
+                cmdEmisor.Parameters.AddWithValue("@RegimenFiscal", emisorElement.Attribute("RegimenFiscal").Value);
+
+                cmdEmisor.ExecuteNonQuery();
+
+                // Guardar Receptor
+                XElement receptorElement = comprobanteElement.Element(XName.Get("Receptor", "http://www.sat.gob.mx/cfd/4"));
+                SqlCommand cmdReceptor = new SqlCommand(
+                    "INSERT INTO Receptor (UUID, Nombre, Rfc, UsoCFDI, DomicilioFiscalReceptor, RegimenFiscalReceptor) " +
+                    "VALUES (@UUID, @Nombre, @Rfc, @UsoCFDI, @DomicilioFiscalReceptor, @RegimenFiscalReceptor)", conn);
+                cmdReceptor.Parameters.AddWithValue("@UUID", timbreElement.Attribute("UUID").Value);
+                cmdReceptor.Parameters.AddWithValue("@Nombre", receptorElement.Attribute("Nombre").Value);
+                cmdReceptor.Parameters.AddWithValue("@Rfc", receptorElement.Attribute("Rfc").Value);
+                cmdReceptor.Parameters.AddWithValue("@UsoCFDI", receptorElement.Attribute("UsoCFDI").Value);
+                cmdReceptor.Parameters.AddWithValue("@DomicilioFiscalReceptor", receptorElement.Attribute("DomicilioFiscalReceptor").Value);
+                cmdReceptor.Parameters.AddWithValue("@RegimenFiscalReceptor", receptorElement.Attribute("RegimenFiscalReceptor").Value);
+
+                cmdReceptor.ExecuteNonQuery();
+
+                // Guardar Conceptos
+                var conceptos = comprobanteElement.Element(XName.Get("Conceptos", "http://www.sat.gob.mx/cfd/4")).Elements(XName.Get("Concepto", "http://www.sat.gob.mx/cfd/4"));
+                foreach (var concepto in conceptos)
+                {
+                    SqlCommand cmdConcepto = new SqlCommand(
+                        "INSERT INTO Concepto (UUID, ClaveProdServ, Cantidad, ClaveUnidad, Unidad, NoIdentificacion, Descripcion, ValorUnitario, Importe, Descuento, ObjetoImp) " +
+                        "VALUES (@UUID, @ClaveProdServ, @Cantidad, @ClaveUnidad, @Unidad, @NoIdentificacion, @Descripcion, @ValorUnitario, @Importe, @Descuento, @ObjetoImp); SELECT SCOPE_IDENTITY();", conn);
+                    cmdConcepto.Parameters.AddWithValue("@UUID", timbreElement.Attribute("UUID").Value);
+                    cmdConcepto.Parameters.AddWithValue("@ClaveProdServ", concepto.Attribute("ClaveProdServ").Value);
+                    cmdConcepto.Parameters.AddWithValue("@Cantidad", decimal.Parse(concepto.Attribute("Cantidad").Value));
+                    cmdConcepto.Parameters.AddWithValue("@ClaveUnidad", concepto.Attribute("ClaveUnidad").Value);
+                    cmdConcepto.Parameters.AddWithValue("@Unidad", concepto.Attribute("Unidad").Value);
+                    cmdConcepto.Parameters.AddWithValue("@NoIdentificacion", concepto.Attribute("NoIdentificacion")?.Value ?? string.Empty);
+                    cmdConcepto.Parameters.AddWithValue("@Descripcion", concepto.Attribute("Descripcion").Value);
+                    cmdConcepto.Parameters.AddWithValue("@ValorUnitario", decimal.Parse(concepto.Attribute("ValorUnitario").Value));
+                    cmdConcepto.Parameters.AddWithValue("@Importe", decimal.Parse(concepto.Attribute("Importe").Value));
+                    cmdConcepto.Parameters.AddWithValue("@Descuento", decimal.Parse(concepto.Attribute("Descuento")?.Value ?? "0"));
+                    cmdConcepto.Parameters.AddWithValue("@ObjetoImp", concepto.Attribute("ObjetoImp").Value);
+
+                    // Insertar el concepto y obtener el ConceptoID insertado
+                    int conceptoID = Convert.ToInt32(cmdConcepto.ExecuteScalar());
+
+                    // Ahora, para los impuestos del concepto:
+                    var impuestosConcepto = concepto.Element(XName.Get("Impuestos", "http://www.sat.gob.mx/cfd/4"));
+                    if (impuestosConcepto != null)
+                    {
+                        var impuestos = impuestosConcepto.Elements(XName.Get("Traslados", "http://www.sat.gob.mx/cfd/4")).Elements(XName.Get("Traslado", "http://www.sat.gob.mx/cfd/4"));
+                        foreach (var impuesto in impuestos)
+                        {
+                            SqlCommand cmdImpuesto = new SqlCommand(
+                                "INSERT INTO Impuesto (ConceptoID, Impuesto, TipoFactor, TasaOCuota, Importe, Base) " +
+                                "VALUES (@ConceptoID, @Impuesto, @TipoFactor, @TasaOCuota, @Importe, @Base)", conn);
+                            cmdImpuesto.Parameters.AddWithValue("@ConceptoID", conceptoID);
+                            cmdImpuesto.Parameters.AddWithValue("@Impuesto", impuesto.Attribute("Impuesto").Value);
+                            cmdImpuesto.Parameters.AddWithValue("@TipoFactor", impuesto.Attribute("TipoFactor").Value);
+                            cmdImpuesto.Parameters.AddWithValue("@TasaOCuota", decimal.Parse(impuesto.Attribute("TasaOCuota").Value));
+                            cmdImpuesto.Parameters.AddWithValue("@Importe", decimal.Parse(impuesto.Attribute("Importe").Value));
+                            cmdImpuesto.Parameters.AddWithValue("@Base", decimal.Parse(impuesto.Attribute("Base").Value));
+
+                            cmdImpuesto.ExecuteNonQuery();
+                        }
+                    }
+                }
+
+                // Guardar Timbre Fiscal Digital
+                SqlCommand cmdTimbre = new SqlCommand(
+                    "INSERT INTO TimbreFiscalDigital (UUID, Version, FechaTimbrado, SelloCFD, NoCertificadoSAT, SelloSAT, RfcProvCertif) " +
+                    "VALUES (@UUID, @Version, @FechaTimbrado, @SelloCFD, @NoCertificadoSAT, @SelloSAT, @RfcProvCertif)", conn);
+                cmdTimbre.Parameters.AddWithValue("@UUID", timbreElement.Attribute("UUID").Value);
+                cmdTimbre.Parameters.AddWithValue("@Version", timbreElement.Attribute("Version").Value);
+                cmdTimbre.Parameters.AddWithValue("@FechaTimbrado", DateTime.Parse(timbreElement.Attribute("FechaTimbrado").Value));
+                cmdTimbre.Parameters.AddWithValue("@SelloCFD", timbreElement.Attribute("SelloCFD").Value);
+                cmdTimbre.Parameters.AddWithValue("@NoCertificadoSAT", timbreElement.Attribute("NoCertificadoSAT").Value);
+                cmdTimbre.Parameters.AddWithValue("@SelloSAT", timbreElement.Attribute("SelloSAT").Value);
+                cmdTimbre.Parameters.AddWithValue("@RfcProvCertif", timbreElement.Attribute("RfcProvCertif").Value);
+
+                cmdTimbre.ExecuteNonQuery();
+            }
+        }
+    }
+}
